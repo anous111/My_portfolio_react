e93bef1a17872a3a307a986f1beabb3bdbb0385d
Anis safia <anissafia90@gmail.com>
2022-01-08 20:07:34 +0100

    Deploy

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: "React Ecome",
+    description:
+      "Baggage store Ecommerce",
+    image: "./E-commerce.PNG",
+    link: "https://condescending-edison-9f7741.netlify.app/?fbclid=IwAR3YOLg0zzBnb1JUg5pp07I5WBGD9Z15HToRm_M9EB-7ztTfYJ9wHgKpmkI",
+  },
+  {
+    title: "React Restaurant",
+    description:
+      "Apps restaurant foods",
+    image: "./Restaurant.PNG",
+    link: "https://heuristic-panini-7d7a17.netlify.app/index.html?fbclid=IwAR2Jam1Bk_zYs-F8IY5fMyuxerm80vmb-4BdBGCEP3sMiH5UAJ5qeGLhXFg",
+  },
+ 
+  
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Html5",
+  "Css3",
+  "Bootstrap4",
+  "Php",
+  "Mysql",
+  "Laravel7",
+  "MVC",
+
+];
